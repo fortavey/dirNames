26c7ead99644b60621fe26766898f67eaac30d61
fortavey <fortavey@gmail.com>
2024-12-14 09:26:59 +0300

    fix errod broken zip

--- unzip.js
+++ unzip.js
@@ -5,15 +5,22 @@ const decompress = require('decompress');
 const filenames = fs.readdirSync(path.join(__dirname, 'sites'));
 
 
-const unzipFiles = () => {
-    filenames.forEach(file => {
-        let newDirName = file;
-        decompress(path.join(__dirname, 'sites', file), 'dist',{
-            map: filee => {
-                filee.path = `${newDirName}*${filee.path}`;
-                return filee;
-            }
-        })
+const unzipFiles =  () => {
+    filenames.forEach(async file => {
+        console.log(file);
+        
+        try {
+            let newDirName = file;
+            await decompress(path.join(__dirname, 'sites', file), 'dist', {
+                map: filee => {
+                    filee.path = `${newDirName}*${filee.path}`;
+                    return filee;
+                }
+            })
+        }catch(err) {
+            console.log('ERROR', file);
+        }
+        
     });
 }
 
